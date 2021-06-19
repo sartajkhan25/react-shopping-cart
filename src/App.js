afaa81999adbe4479e8f959211d99afb5caa0eb0
Sartaj Khan <sartaj2493@gmail.com>
2021-06-19 18:38:30 +0530

    Cart Component Done2

--- src/App.js
+++ src/App.js
@@ -2,30 +2,60 @@ import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
+import Cart from "./components/Cart";
+import { thisExpression } from "@babel/types";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     };
   }
-
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice(); //clone copy
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
   sortProducts = (event) => {
     console.log(event.target.value);
-    const sort = event.target.value
-    this.setState((state)=>({
+    const sort = event.target.value;
+    this.setState((state) => ({
       sort: sort,
-      products: this.state.products.slice().sort((a,b)=>(
-        sort ==="lowest"?
-        ((a.price > b.price)? 1 : -1):
-        sort ==="highest"?
-        ((a.price < b.price)? 1 : -1):
-        ((a._id > b._id)? 1 : -1)
-      ))
-    }))
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
     // switch (event.target.value) {
     //   case "":
     //     return this.setState({
@@ -72,9 +102,14 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products}></Products>
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              ></Products>
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+            </div>
           </div>
         </main>
         <footer>All Right Reserved</footer>
